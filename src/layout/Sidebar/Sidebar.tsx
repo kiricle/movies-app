4bfc774a6138abf7645d50ec0fd3b4b7485681fa
Kolotylo Kyrylo <kolot102@gmail.com>
2023-04-10 15:43:14 +0300

    add searchbar to sidebar

--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -1,11 +1,13 @@
 import { Logo } from '@/components/Logo/Logo';
 import { Menu } from '../Menu/Menu';
+import { SearchBar } from './SearchBar/SearchBar';
 import { SidebarProps } from './Sidebar.props';
 
 export const Sidebar = ({ ...props }: SidebarProps) => {
     return (
         <aside {...props}>
             <Logo />
+            <SearchBar />
             <Menu />
         </aside>
     );
